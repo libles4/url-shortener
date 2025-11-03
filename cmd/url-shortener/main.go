03a829b175633a2960f541254063206a65c132dd
Alexey Mokeev <work.alexey.mokeev@gmail.com>
2025-11-03 19:54:50 +0400

    feat: add HTTP server with chi router and URL save handler

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"fmt"
 	"log/slog"
+	"net/http"
 	"os"
 	"url-shortener/internal/config"
+	"url-shortener/internal/http-server/handlers/url/save"
 	"url-shortener/internal/lib/logger/sl"
 	"url-shortener/internal/storage/sqlite"
+
+	mwLogger "url-shortener/internal/http-server/middleware/logger"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 const (
@@ -28,9 +35,33 @@ func main() {
 	fmt.Println(id, err)
 	url, err := storage.GetURL("ts.yo")
 	fmt.Println(url, err)
+
+	router := chi.NewRouter()
+	router.Use(middleware.RequestID)
+	router.Use(middleware.Logger)
+	router.Use(mwLogger.New(log))
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
+
+	router.Post("/url", save.New(log, storage))
+
+	log.Info("stating server", slog.String("address", cfg.Address))
+
+	srv := &http.Server{
+		Addr:         cfg.Address,
+		Handler:      router,
+		ReadTimeout:  cfg.HTTPServer.Timeout,
+		WriteTimeout: cfg.HTTPServer.Timeout,
+		IdleTimeout:  cfg.HTTPServer.IdleTimeout,
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		log.Error("failed to start server")
+	}
+
 }
 
-func setupLogger(env string) slog.Logger {
+func setupLogger(env string) *slog.Logger {
 	var log *slog.Logger
 	var handler slog.Handler
 	switch env {
@@ -42,5 +73,5 @@ func setupLogger(env string) slog.Logger {
 		handler = slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo})
 	}
 	log = slog.New(handler)
-	return *log
+	return log
 }
